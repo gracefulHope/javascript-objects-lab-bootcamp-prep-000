9e89f20b2971252b5eab9e5517cbafeb0ec2bf27
Sara S <ewf.gloria@gmail.com>
2018-02-07 20:32:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {}
 function updateObjectWithKeyAndValue(thing, key, value){
   var newThing = Object.assign({}, thing)
   Object.assign(newThing, {[key]: value})
-  return thing
+  return newThing
 }
 
